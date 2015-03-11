6e5e13c88b68bb92cceb2a8ed875b8e3b3754050
Alex Hughes <alex@customsoftuk.com>
2015-03-11 14:03:41 +0200

    External settings files for each platform

---
+++ live_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'vhcGccovggJ2b+71zs0gAqu3KeWBhc8JzPy60KNrKlZq'
+
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = set(['DOC', 'DOCX', 'XLS', 'XLSX', 
+                          'ODT', 'PAGES', 'WPD', 'WPS', 
+                          'RTF', 'PDF', 'TEX', 'TXT'])
+
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'alex@customsoftuk.com'
+MAIL_PASSWORD = 'Custom Soft Hero!'
